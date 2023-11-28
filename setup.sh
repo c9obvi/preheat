23e5bd13b6f39e45f18dc111177915ddaa1507ce
0xBerto.Ξ <31329416+c9obvi@users.noreply.github.com>
2023-11-27 16:07:46 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 # Update the system
-echo "*****Updating system..."
+echo "*****Updating system ➡️ ..."
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install Git
-echo "*****Installing Git..."
+echo "*****Installing Git 🌳..."
 sudo apt-get install git -y
 
 # Install Node.js
-echo "*****Installing Node.js..."
+echo "*****Installing Node.js 💻..."
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 
 # Install Python3 and pip (if not already installed)
-echo "*****Installing Python3 and pip..."
+echo "*****Installing Python3 and pip 🐍..."
 sudo apt-get install python3 python3-pip -y
 
 # Install Flask
-echo "*****Installing Flask..."
+echo "*****Installing Flask 🍺..."
 pip3 install Flask
 
 # Install BeautifulSoup
-echo "*****Installing BeautifulSoup..."
+echo "*****Installing BeautifulSoup 🍜..."
 pip3 install beautifulsoup4
 
 # Install Selenium
-echo "*****Installing Selenium..."
+echo "*****Installing Selenium 🛜..."
 pip3 install selenium
 
 # Install additional dependencies for Selenium (like webdriver)
 # Uncomment and modify the following line according to your specific needs
 # sudo apt-get install <dependency-name> -y
 
-echo "___~~~*****Setup complete!*****~~~___"
+echo "___~~~***** 🫡 Setup complete!🚀🚀🚀*****~~~___"
